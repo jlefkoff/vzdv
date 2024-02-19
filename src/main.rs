6e266187a3760195dd5cce6a5455cb23a5b39250
Celeo <mattboulanger@fastmail.com>
2024-02-19 15:10:39 -0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use crate::{config::Config, shared::AppState};
+use crate::shared::{AppState, Config};
 use anyhow::Result;
 use axum::{routing::get, Router};
+use log::{debug, error, info};
 use minijinja::Environment;
 use sqlx::{sqlite::SqliteConnectOptions, Executor, SqlitePool};
-use std::{path::Path, sync::Arc};
+use std::{env, path::Path, sync::Arc};
 
-mod config;
 mod endpoints;
 mod shared;
 mod sql;
 
 fn load_config() -> Result<Config> {
-    let text = std::fs::read_to_string("./config.toml")?;
+    let text = std::fs::read_to_string(shared::CONFIG_FILE_NAME)?;
     let config: Config = toml::from_str(&text)?;
     Ok(config)
 }
@@ -39,23 +39,42 @@ async fn load_db(config: &Config) -> Result<SqlitePool> {
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let config = load_config()?;
-    let db = load_db(&config).await?;
-    let templates = load_templates();
+async fn main() {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info,new_site=debug");
+    }
+    pretty_env_logger::init();
 
+    debug!("Loading");
+    let config = match load_config() {
+        Ok(c) => c,
+        Err(e) => {
+            error!("Could not load config: {e}");
+            std::process::exit(1);
+        }
+    };
+    let db = match load_db(&config).await {
+        Ok(db) => db,
+        Err(e) => {
+            error!("Could not load DB: {e}");
+            std::process::exit(1);
+        }
+    };
+    let templates = load_templates();
+    debug!("Loaded");
+
+    debug!("Setting up app");
     let app_state = Arc::new(AppState {
         config,
         db,
         templates,
     });
-
     let app = Router::new()
         .route("/", get(endpoints::handler_home))
         .with_state(app_state);
+    debug!("Set up");
 
+    info!("Listening on http://0.0.0.0:3000/");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
-    Ok(())
 }
--- src/shared.rs
+++ src/shared.rs
@@ -1,9 +1,21 @@
-use crate::config::Config;
 use minijinja::Environment;
+use serde::Deserialize;
 use sqlx::SqlitePool;
 
 pub struct AppState {
     pub config: Config,
     pub db: SqlitePool,
     pub templates: Environment<'static>,
+}
+
+pub const CONFIG_FILE_NAME: &str = "site_config.toml";
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub database: ConfigDatabase,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ConfigDatabase {
+    pub file: String,
 }
