46ab1296e32061f913d435d0f0a8b03ce33fba4c
Celeo <mattboulanger@fastmail.com>
2024-02-24 21:00:37 -0800

    Work on sessions

--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -3,6 +3,7 @@
 #![allow(unused)]
 
 use minijinja::Environment;
+use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
 
 mod config;
@@ -14,6 +15,18 @@ pub struct AppState {
     pub config: config::Config,
     pub db: SqlitePool,
     pub templates: Environment<'static>,
+}
+
+/// Key for user info CRUD in session.
+pub const SESSION_USER_INFO_KEY: &str = "USER_INFO";
+
+/// Data stored in the user's session.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UserInfo {
+    session_id: String,
+    cid: u32,
+    first_name: String,
+    last_name: String,
 }
 
 #[allow(clippy::upper_case_acronyms)]
