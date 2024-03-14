4092f3a30a4a2739075f271eaca137c41594a983
Celeo <mattboulanger@fastmail.com>
2024-03-14 10:48:57 -0700

    Make `Config` `.clone`-able

--- src/shared/config.rs
+++ src/shared/config.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 pub const DEFAULT_CONFIG_FILE_NAME: &str = "site_config.toml";
 
 /// App configuration.
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Config {
     pub database: ConfigDatabase,
     pub vatsim: ConfigVatsim,
@@ -13,26 +13,27 @@ pub struct Config {
     pub webhooks: ConfigWebhooks,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ConfigDatabase {
     pub file: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ConfigVatsim {
     pub oauth_client_id: String,
     pub oauth_client_secret: String,
     pub oauth_client_callback_url: String,
     pub vatusa_facility_code: String,
+    pub vatusa_api_key: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ConfigAirports {
     pub all: Vec<Airport>,
     pub weather_for: Vec<String>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Airport {
     pub code: String,
     pub name: String,
@@ -41,13 +42,13 @@ pub struct Airport {
     pub class: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ConfigStats {
     pub position_prefixes: Vec<String>,
     pub position_suffixes: Vec<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ConfigWebhooks {
     pub staffing_request: String,
     pub feedback: String,
